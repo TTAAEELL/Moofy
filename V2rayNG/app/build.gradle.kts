385b41b4d06bfccce8c6e18ebae88089b02074dc
TAEL <ttaaeell@gmail.com>
2025-02-24 00:43:02 +0330

    update

--- V2rayNG/app/build.gradle.kts
+++ V2rayNG/app/build.gradle.kts
@@ -109,7 +109,7 @@ android {
                     else
                         "universal"
 
-                output.outputFileName = "NikaNG_${variant.versionName}_${abi}.apk"
+                output.outputFileName = "Moofy_${variant.versionName}_${abi}.apk"
                 if (versionCodes.containsKey(abi)) {
                     output.versionCodeOverride = (1000000 * versionCodes[abi]!!).plus(variant.versionCode)
                 } else {
